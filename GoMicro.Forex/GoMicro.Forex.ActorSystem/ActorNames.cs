9fb2c5f93787e3bc3a55802f38c3ea92c2ea6fa5
George Albertyn <george.albertyn@gmail.com>
2017-05-25 14:40:40 +0200

    Actor System

---
+++ GoMicro.Forex/GoMicro.Forex.ActorSystem/ActorNames.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoMicro.Forex.ActorSystem
+{
+    public class ActorNames
+    {
+        public const string Ping = @"ping";
+        public const string Quartz = @"quartz";
+        public const string Fixer = @"fixer";
+        public const string Quote = @"quote";
+        public const string Crud = @"crud";
+    }
+}
